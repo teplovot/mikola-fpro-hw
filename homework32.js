3f5b31c6803ae040b5456bf80bd6e73cfc27d0b8
Mikola Grishaev <teplovot@gmail.com>
2023-08-19 06:40:13 +0300

    1908 640

--- homework32.js
+++ homework32.js
@@ -4,3 +4,27 @@
 питаємо у користувача перше число;
 запитуємо у користувача друге число;
 виводимо результат дії (add, sub, mult, div) з усіма операндами (Наприклад "2 + 3 = 5").*/
+let question = prompt(`Що ти хочеi зробити (+, -, *, /)`);
+let number1 = prompt(`Питаємо у користувача перше число`, 'Введіть перше число');
+let number2 = prompt(`Питаємо у користувача друге число`, 'Введіть друге число');
+
+let result = 0;
+
+switch (question) {
+    case "+":
+        result = Number(number1) + Number(number2);
+        break;
+    case "-":
+        result = number1 - number2;
+        break;
+    case "*":
+        result = number1 * number2;
+        break;
+    case "/":
+        result = number1 / number2;
+        break;
+    default:
+        break;
+}
+
+alert(`${number1}${question}${number2}=${result}`);
